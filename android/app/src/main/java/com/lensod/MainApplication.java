51b2abe559bc76aeeabeb296d26ad834f32a31a2
john <kodchanat2@gmail.com>
2017-12-13 19:36:35 +0700

    remove(react-native-collapsing-toolbar) it can't use

--- android/app/src/main/java/com/lensod/MainApplication.java
+++ android/app/src/main/java/com/lensod/MainApplication.java
@@ -8,8 +8,6 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.rnnestedscrollview.RNNestedScrollViewPackage;
-import com.rncollapsingtoolbar.RNCollapsingToolbarPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,9 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
-          new RNCollapsingToolbarPackage(),
-          new RNNestedScrollViewPackage()
+            new VectorIconsPackage()
       );
     }
 
