f7c319db0bca3a1e280c898a21e22747e7b1c599
john <kodchanat2@gmail.com>
2017-12-15 18:27:10 +0700

    mock (duplicate mock)

--- config.js
+++ config.js
@@ -21,7 +21,127 @@ export const list = [
 ปณิธิ สุขสายชล (บาส) เบส`,
         genres: ['POP'],
         otherpic: [
-            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/9e5ca743-3799-493a-9f56-794af2e7d980.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/6c623d17-f52b-4855-bc78-9c018b2ab9ba.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/380a46a7-a5d3-4719-9294-68a957b722ce.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/d0af41b0-bcb5-4ff4-90a8-275570642bb5.jpeg"
+        ],
+    },
+    {
+        title: "ZWEEDZ N' ROLL",
+        pic: "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/1d784562-ce8d-41b2-93b4-d272f378d0cf.jpeg",
+        description: `Zweedz n' roll (Alternative/Rock)
+Zweedz n' roll, an alternative band from the Faculty of Music, Silpakorn University,
+was founded in 2012 by a group of friends who have passion in Brit music. At first,
+we wanted to play in a pub at night just for fun, but after playing for several gigs,
+one of the pub owners inspired us to become much more. Since then we started to
+cover songs and post our clips on Youtube, which became a good channel for fans to follow us.
+Inspired by Brit Pop/Rock and alternative bands like The Beatles, The Cranberries,
+Radiohead and Nirvana, ZWEEDZ n' roll was named by New (the bassist).
+The name came from "rock n' roll" and "sweet n' roll" and finally became
+"ZWEEDZ n' roll" The debut of an MV "December"
+ZWEEDZ n' Roll วงอัลเตอร์เนทีฟ จากรั้วมหาลัยศิลปากร คณะดุริยางคศาสตร์
+เริ่มก่อตั้งเมื่อปี 2012เป็นการรวมตัวของกลุ่มเพื่อนที่มีความหลงใหลดนตรีจากฝั่งอังกฤษ เพื่อเล่นดนตรีกลางคืน
+หลังจากนั้นก็ได้แกะเพลงตระเวณตามร้านต่างๆ ที่เปิดรับแนวดนตรีที่เราอยากจะเล่น จนได้Auditionที่ร้านๆนึง
+ความคิดของทางวงก็เริ่มเปลี่ยนไป จากการแนะนำของพี่เจ้าของร้าน หลังจากนั้นจึงเริ่มอัดคลิปCoverเพลงต่างๆ
+ลงเว็บไซต์ YOUTUBE และก็ได้มีผลงานของตัวเองในที่สุดคือเพลง "ธันวาคม - zweedz n' roll"
+Zweedz n' roll เป็นชื่อที่นิว(เบส) เป็นคนตั้งขึ้น เกิดจากการเล่นคำจาก rock n' roll >> sweet n' roll
+และเป็น zweedz n' roll ในที่สุด ได้รับอิทธิพลทางดนตรีหลักๆจากดนตรีแนว Brit Pop/Rock และAlternativeต่างๆ
+เช่น The Beatles ,The Cranberries ,Radiohead และ Nirvana เป็นต้น
+ประจำที่
+PLAY YARD by Studio Bar ลาดพร้าวซอย8 วันพฤหัสเว้นพฤหัสนะ :-)`,
+        genres: ['Alternative'],
+        otherpic: [
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/3efe484d-6117-48b9-89a9-68a69637c51f.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/2c63410b-aeb3-4177-96d9-4ad4d2668806.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/8d9d315c-adbc-4346-9587-e0aa8576dd5c.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/be2585b9-484b-40a4-b74a-e51fec7440a6.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/8ed20559-4766-473a-924e-57f9b73bb914.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/7e9ccff2-5c3d-4668-b090-6d5529770087.jpeg"
+        ],
+    },
+    {
+        title: "RIVER RHYME",
+        pic: "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/f1fc7ee5-48b6-46c1-9668-a145cbe1a6c9.jpeg",
+        otherpic: [
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/7a2c948c-9081-46d9-b7d7-0524bce1f143.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/5d8511aa-522d-4c3c-af17-32af8e4bbdb1.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/51cb1b16-7aea-40e6-b17c-1a9feabae613.jpeg"
+        ],
+        description: `วุฒิชัย เอมแสง / Vutthichai Aemsang (จอห์น / John) aka. JOHNN: Rapper ธนภัทร พุทธกะ / Thanapat Putthaka (ต็อป / Stop) aka. BLACKSHEEPRR: Rapper นครินทร์ จรูญวิทยา / Nakarin Jalunvitaya (แชมป์ / Champ) aka. MAIYARAP: Rapper “ชื่อกูจำง่ายแค่สามพยางค์” RIVER RHYME กลุ่มศิลปินฮิปฮอปคุณภาพที่โด่งดังจากการขึ้นสังเวียนแร็ปเปอร์ที่เดือดดาลที่สุดของไทย “RAP IS NOW” เมื่อปี 2556 ปัจจุบัน RIVER RHYME ประกอบด้วยสมาชิกทั้งหมด 3 คน คือ BLACKSHEEPRR (ต็อป) MAIYARAP (แชมป์) และ JOHNN (จอห์น) ความโดดเด่นของ RIVER RHYME คือ เนื้อเพลงที่ฟังง่าย และสื่อสารออกมาได้อย่างตรงไปตรงมา โดยในปีนี้ทั้งสามคนอยู่ระหว่างการร่วมกันทำเพลงใหม่ ๆ ออกมาให้ทุกคนได้ติดตามกันตลอดทั้งปี “Let the RHYME flow” RIVER RHYME, also known simply as RR, is a group of 3 young talented Rap/Hip-hop artists in Thailand, BLACKSHEEPRR (Top), MAIYARAP (Champ) and JOHNN (John). RR rose to fame following the success of their performance on a televised rap battle series called “RAP IS NOW” in 2013. Since then, RR has produced and released more than 10 records and will continue to release many more this year.`,
+        genres: ['HipHop/Rap', 'R&B']
+    },
+    {
+        title: "Nap A Lean",
+        pic: "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/9e5ca743-3799-493a-9f56-794af2e7d980.jpeg",
+        description: `ศิลปินดูโอ้จากเชียงใหม่ เจ้าของรางวัล พลัดถิ่นยอดเยี่ยม minimal awards #1 2013 โดยมีโต้มือกีต้าร์ Acoustic ผู้เป็นนักร้องนำ แล้วฮั้วมือกีต้าร์ไฟฟ้า ที่มาเจอกันในร้านอาหารที่ฮั้วทำงานอยู่ โดยในช่วงแรกของการทำเพลง ทั้งคู่ทำเองทุกอย่าง ไม่ว่าจะเป็นการอัดเสียง ทำเฟซบุ๊กวง ทำอาร์ตเวิร์ค โปสเตอร์โปรโมท ไปจนถึงนำไฟล์เพลงไปส่งคลื่นวิทยุ และทั้งสองก็เป็นเจ้าของเพลงเพราะๆอย่าง ความรักที่ซ่อนไว้ และ ไม่เป็นไรหรอก โดยนอกจากการเป็นศิลปินแล้ว ทั้งสองยังเป็นครีเอทีฟในบ้านของ Spicy Discs อีกด้วย 
+
+ธนพล ทองสวัสดิ์ (โต้) ร้องนำ
+พิสิฐ สมบัติพินพง (ฮั้ว) กีต้าร์
+นภัสรพี ยาอินทร์ (ตอง) กลอง
+ปณิธิ สุขสายชล (บาส) เบส`,
+        genres: ['POP'],
+        otherpic: [
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/6c623d17-f52b-4855-bc78-9c018b2ab9ba.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/380a46a7-a5d3-4719-9294-68a957b722ce.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/d0af41b0-bcb5-4ff4-90a8-275570642bb5.jpeg"
+        ],
+    },
+    {
+        title: "ZWEEDZ N' ROLL",
+        pic: "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/1d784562-ce8d-41b2-93b4-d272f378d0cf.jpeg",
+        description: `Zweedz n' roll (Alternative/Rock)
+Zweedz n' roll, an alternative band from the Faculty of Music, Silpakorn University,
+was founded in 2012 by a group of friends who have passion in Brit music. At first,
+we wanted to play in a pub at night just for fun, but after playing for several gigs,
+one of the pub owners inspired us to become much more. Since then we started to
+cover songs and post our clips on Youtube, which became a good channel for fans to follow us.
+Inspired by Brit Pop/Rock and alternative bands like The Beatles, The Cranberries,
+Radiohead and Nirvana, ZWEEDZ n' roll was named by New (the bassist).
+The name came from "rock n' roll" and "sweet n' roll" and finally became
+"ZWEEDZ n' roll" The debut of an MV "December"
+ZWEEDZ n' Roll วงอัลเตอร์เนทีฟ จากรั้วมหาลัยศิลปากร คณะดุริยางคศาสตร์
+เริ่มก่อตั้งเมื่อปี 2012เป็นการรวมตัวของกลุ่มเพื่อนที่มีความหลงใหลดนตรีจากฝั่งอังกฤษ เพื่อเล่นดนตรีกลางคืน
+หลังจากนั้นก็ได้แกะเพลงตระเวณตามร้านต่างๆ ที่เปิดรับแนวดนตรีที่เราอยากจะเล่น จนได้Auditionที่ร้านๆนึง
+ความคิดของทางวงก็เริ่มเปลี่ยนไป จากการแนะนำของพี่เจ้าของร้าน หลังจากนั้นจึงเริ่มอัดคลิปCoverเพลงต่างๆ
+ลงเว็บไซต์ YOUTUBE และก็ได้มีผลงานของตัวเองในที่สุดคือเพลง "ธันวาคม - zweedz n' roll"
+Zweedz n' roll เป็นชื่อที่นิว(เบส) เป็นคนตั้งขึ้น เกิดจากการเล่นคำจาก rock n' roll >> sweet n' roll
+และเป็น zweedz n' roll ในที่สุด ได้รับอิทธิพลทางดนตรีหลักๆจากดนตรีแนว Brit Pop/Rock และAlternativeต่างๆ
+เช่น The Beatles ,The Cranberries ,Radiohead และ Nirvana เป็นต้น
+ประจำที่
+PLAY YARD by Studio Bar ลาดพร้าวซอย8 วันพฤหัสเว้นพฤหัสนะ :-)`,
+        genres: ['Alternative'],
+        otherpic: [
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/3efe484d-6117-48b9-89a9-68a69637c51f.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/2c63410b-aeb3-4177-96d9-4ad4d2668806.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/8d9d315c-adbc-4346-9587-e0aa8576dd5c.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/be2585b9-484b-40a4-b74a-e51fec7440a6.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/8ed20559-4766-473a-924e-57f9b73bb914.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/7e9ccff2-5c3d-4668-b090-6d5529770087.jpeg"
+        ],
+    },
+    {
+        title: "RIVER RHYME",
+        pic: "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/f1fc7ee5-48b6-46c1-9668-a145cbe1a6c9.jpeg",
+        otherpic: [
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/7a2c948c-9081-46d9-b7d7-0524bce1f143.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/5d8511aa-522d-4c3c-af17-32af8e4bbdb1.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/51cb1b16-7aea-40e6-b17c-1a9feabae613.jpeg"
+        ],
+        description: `วุฒิชัย เอมแสง / Vutthichai Aemsang (จอห์น / John) aka. JOHNN: Rapper ธนภัทร พุทธกะ / Thanapat Putthaka (ต็อป / Stop) aka. BLACKSHEEPRR: Rapper นครินทร์ จรูญวิทยา / Nakarin Jalunvitaya (แชมป์ / Champ) aka. MAIYARAP: Rapper “ชื่อกูจำง่ายแค่สามพยางค์” RIVER RHYME กลุ่มศิลปินฮิปฮอปคุณภาพที่โด่งดังจากการขึ้นสังเวียนแร็ปเปอร์ที่เดือดดาลที่สุดของไทย “RAP IS NOW” เมื่อปี 2556 ปัจจุบัน RIVER RHYME ประกอบด้วยสมาชิกทั้งหมด 3 คน คือ BLACKSHEEPRR (ต็อป) MAIYARAP (แชมป์) และ JOHNN (จอห์น) ความโดดเด่นของ RIVER RHYME คือ เนื้อเพลงที่ฟังง่าย และสื่อสารออกมาได้อย่างตรงไปตรงมา โดยในปีนี้ทั้งสามคนอยู่ระหว่างการร่วมกันทำเพลงใหม่ ๆ ออกมาให้ทุกคนได้ติดตามกันตลอดทั้งปี “Let the RHYME flow” RIVER RHYME, also known simply as RR, is a group of 3 young talented Rap/Hip-hop artists in Thailand, BLACKSHEEPRR (Top), MAIYARAP (Champ) and JOHNN (John). RR rose to fame following the success of their performance on a televised rap battle series called “RAP IS NOW” in 2013. Since then, RR has produced and released more than 10 records and will continue to release many more this year.`,
+        genres: ['HipHop/Rap', 'R&B']
+    },
+    {
+        title: "Nap A Lean",
+        pic: "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/9e5ca743-3799-493a-9f56-794af2e7d980.jpeg",
+        description: `ศิลปินดูโอ้จากเชียงใหม่ เจ้าของรางวัล พลัดถิ่นยอดเยี่ยม minimal awards #1 2013 โดยมีโต้มือกีต้าร์ Acoustic ผู้เป็นนักร้องนำ แล้วฮั้วมือกีต้าร์ไฟฟ้า ที่มาเจอกันในร้านอาหารที่ฮั้วทำงานอยู่ โดยในช่วงแรกของการทำเพลง ทั้งคู่ทำเองทุกอย่าง ไม่ว่าจะเป็นการอัดเสียง ทำเฟซบุ๊กวง ทำอาร์ตเวิร์ค โปสเตอร์โปรโมท ไปจนถึงนำไฟล์เพลงไปส่งคลื่นวิทยุ และทั้งสองก็เป็นเจ้าของเพลงเพราะๆอย่าง ความรักที่ซ่อนไว้ และ ไม่เป็นไรหรอก โดยนอกจากการเป็นศิลปินแล้ว ทั้งสองยังเป็นครีเอทีฟในบ้านของ Spicy Discs อีกด้วย 
+
+ธนพล ทองสวัสดิ์ (โต้) ร้องนำ
+พิสิฐ สมบัติพินพง (ฮั้ว) กีต้าร์
+นภัสรพี ยาอินทร์ (ตอง) กลอง
+ปณิธิ สุขสายชล (บาส) เบส`,
+        genres: ['POP'],
+        otherpic: [
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/6c623d17-f52b-4855-bc78-9c018b2ab9ba.jpeg",
             "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/380a46a7-a5d3-4719-9294-68a957b722ce.jpeg",
             "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/d0af41b0-bcb5-4ff4-90a8-275570642bb5.jpeg"
         ],
