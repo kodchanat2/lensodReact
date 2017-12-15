a1b5f14afd81f4f6ddda2c9445ab68241b9018c9
john <kodchanat2@gmail.com>
2017-12-15 11:31:05 +0700

    feat(back route): can press back button

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { StyleSheet, View} from 'react-native';
-import { Router, Scene } from 'react-native-router-flux';
+import { Router, Scene, Actions } from 'react-native-router-flux';
 
 import Landing from './components/landing';
 import Listcard from './components/listcard';
 
 export default class App extends React.Component {
   render() {
+    const onBackAndroid = () => {
+      let curr = Actions.currentScene;
+      Actions.pop();
+      return curr != Actions.currentScene; 
+  };
     return (
       <View style={styles.container}>
-      <Router sceneStyle={styles.scenes}>
+      <Router sceneStyle={styles.scenes} backAndroidHandler={onBackAndroid}>
           <Scene key="root">
               <Scene key="Landing" component={Landing} initial="true" hideNavBar="true" />
               <Scene key="Listcard" component={Listcard} hideNavBar="true"/>
