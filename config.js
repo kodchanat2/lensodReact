5beb2e525992f3af679777f5152f0b1ea7ff2ef9
john <kodchanat2@gmail.com>
2017-12-15 14:37:18 +0700

    mock(other pic)

--- config.js
+++ config.js
@@ -3,6 +3,49 @@ export const list = [
         title: "RIVER RHYME",
         link: "RIVER_RHYME",
         pic: "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/f1fc7ee5-48b6-46c1-9668-a145cbe1a6c9.jpeg",
+        otherpic: [
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/7a2c948c-9081-46d9-b7d7-0524bce1f143.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/5d8511aa-522d-4c3c-af17-32af8e4bbdb1.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/51cb1b16-7aea-40e6-b17c-1a9feabae613.jpeg"
+        ],
+        description: "วุฒิชัย เอมแสง / Vutthichai Aemsang (จอห์น / John) aka. JOHNN: Rapper ธนภัทร พุทธกะ / Thanapat Putthaka (ต็อป / Stop) aka. BLACKSHEEPRR: Rapper นครินทร์ จรูญวิทยา /...",
+        genres: ['HipHop/Rap', 'R&B']
+    },
+    {
+        title: "Nap A Lean",
+        link: "Nap_A_Lean",
+        pic: "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/9e5ca743-3799-493a-9f56-794af2e7d980.jpeg",
+        description: "ศิลปินดูโอ้จากเชียงใหม่ เจ้าของรางวัล พลัดถิ่นยอดเยี่ยม minimal awards #1 2013 โดยมีโต้มือกีต้าร์ Acoustic ผู้เป็นนักร้องนำ แล้วฮั้วมือกีต้าร์ไฟฟ้า ที่มาเจอกันในร้านอาหารที่ฮั้วทำงานอยู่ โดย ...",
+        genres: ['POP']
+    },
+    {
+        title: "RIVER RHYME",
+        link: "RIVER_RHYME",
+        pic: "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/f1fc7ee5-48b6-46c1-9668-a145cbe1a6c9.jpeg",
+        otherpic: [
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/7a2c948c-9081-46d9-b7d7-0524bce1f143.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/5d8511aa-522d-4c3c-af17-32af8e4bbdb1.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/51cb1b16-7aea-40e6-b17c-1a9feabae613.jpeg"
+        ],
+        description: "วุฒิชัย เอมแสง / Vutthichai Aemsang (จอห์น / John) aka. JOHNN: Rapper ธนภัทร พุทธกะ / Thanapat Putthaka (ต็อป / Stop) aka. BLACKSHEEPRR: Rapper นครินทร์ จรูญวิทยา /...",
+        genres: ['HipHop/Rap', 'R&B']
+    },
+    {
+        title: "Nap A Lean",
+        link: "Nap_A_Lean",
+        pic: "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/9e5ca743-3799-493a-9f56-794af2e7d980.jpeg",
+        description: "ศิลปินดูโอ้จากเชียงใหม่ เจ้าของรางวัล พลัดถิ่นยอดเยี่ยม minimal awards #1 2013 โดยมีโต้มือกีต้าร์ Acoustic ผู้เป็นนักร้องนำ แล้วฮั้วมือกีต้าร์ไฟฟ้า ที่มาเจอกันในร้านอาหารที่ฮั้วทำงานอยู่ โดย ...",
+        genres: ['POP']
+    },
+    {
+        title: "RIVER RHYME",
+        link: "RIVER_RHYME",
+        pic: "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/f1fc7ee5-48b6-46c1-9668-a145cbe1a6c9.jpeg",
+        otherpic: [
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/7a2c948c-9081-46d9-b7d7-0524bce1f143.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/5d8511aa-522d-4c3c-af17-32af8e4bbdb1.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/51cb1b16-7aea-40e6-b17c-1a9feabae613.jpeg"
+        ],
         description: "วุฒิชัย เอมแสง / Vutthichai Aemsang (จอห์น / John) aka. JOHNN: Rapper ธนภัทร พุทธกะ / Thanapat Putthaka (ต็อป / Stop) aka. BLACKSHEEPRR: Rapper นครินทร์ จรูญวิทยา /...",
         genres: ['HipHop/Rap', 'R&B']
     },
@@ -14,48 +57,6 @@ export const list = [
         genres: ['POP']
     },
 
-    {
-        title: "RIVER RHYME",
-        link: "RIVER_RHYME",
-        pic: "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/f1fc7ee5-48b6-46c1-9668-a145cbe1a6c9.jpeg",
-        description: "วุฒิชัย เอมแสง / Vutthichai Aemsang (จอห์น / John) aka. JOHNN: Rapper ธนภัทร พุทธกะ / Thanapat Putthaka (ต็อป / Stop) aka. BLACKSHEEPRR: Rapper นครินทร์ จรูญวิทยา /...",
-        genres: ['HipHop/Rap', 'R&B']
-    },
-    {
-        title: "Nap A Lean",
-        link: "Nap_A_Lean",
-        pic: "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/9e5ca743-3799-493a-9f56-794af2e7d980.jpeg",
-        description: "ศิลปินดูโอ้จากเชียงใหม่ เจ้าของรางวัล พลัดถิ่นยอดเยี่ยม minimal awards #1 2013 โดยมีโต้มือกีต้าร์ Acoustic ผู้เป็นนักร้องนำ แล้วฮั้วมือกีต้าร์ไฟฟ้า ที่มาเจอกันในร้านอาหารที่ฮั้วทำงานอยู่ โดย ...",
-        genres: ['POP']
-    },
-    {
-        title: "RIVER RHYME",
-        link: "RIVER_RHYME",
-        pic: "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/f1fc7ee5-48b6-46c1-9668-a145cbe1a6c9.jpeg",
-        description: "วุฒิชัย เอมแสง / Vutthichai Aemsang (จอห์น / John) aka. JOHNN: Rapper ธนภัทร พุทธกะ / Thanapat Putthaka (ต็อป / Stop) aka. BLACKSHEEPRR: Rapper นครินทร์ จรูญวิทยา /...",
-        genres: ['HipHop/Rap', 'R&B']
-    },
-    {
-        title: "Nap A Lean",
-        link: "Nap_A_Lean",
-        pic: "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/9e5ca743-3799-493a-9f56-794af2e7d980.jpeg",
-        description: "ศิลปินดูโอ้จากเชียงใหม่ เจ้าของรางวัล พลัดถิ่นยอดเยี่ยม minimal awards #1 2013 โดยมีโต้มือกีต้าร์ Acoustic ผู้เป็นนักร้องนำ แล้วฮั้วมือกีต้าร์ไฟฟ้า ที่มาเจอกันในร้านอาหารที่ฮั้วทำงานอยู่ โดย ...",
-        genres: ['POP']
-    },
-    {
-        title: "RIVER RHYME",
-        link: "RIVER_RHYME",
-        pic: "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/f1fc7ee5-48b6-46c1-9668-a145cbe1a6c9.jpeg",
-        description: "วุฒิชัย เอมแสง / Vutthichai Aemsang (จอห์น / John) aka. JOHNN: Rapper ธนภัทร พุทธกะ / Thanapat Putthaka (ต็อป / Stop) aka. BLACKSHEEPRR: Rapper นครินทร์ จรูญวิทยา /...",
-        genres: ['HipHop/Rap', 'R&B']
-    },
-    {
-        title: "Nap A Lean",
-        link: "Nap_A_Lean",
-        pic: "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/9e5ca743-3799-493a-9f56-794af2e7d980.jpeg",
-        description: "ศิลปินดูโอ้จากเชียงใหม่ เจ้าของรางวัล พลัดถิ่นยอดเยี่ยม minimal awards #1 2013 โดยมีโต้มือกีต้าร์ Acoustic ผู้เป็นนักร้องนำ แล้วฮั้วมือกีต้าร์ไฟฟ้า ที่มาเจอกันในร้านอาหารที่ฮั้วทำงานอยู่ โดย ...",
-        genres: ['POP']
-    },
 ]
 
 export const eventData = [
