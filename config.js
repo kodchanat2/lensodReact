a51601565ab66c09fe99e31b42dfe7605c134f66
john <kodchanat2@gmail.com>
2017-12-15 16:45:37 +0700

    mock( more band)

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 export const list = [
     {
         title: "RIVER RHYME",
-        link: "RIVER_RHYME",
         pic: "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/f1fc7ee5-48b6-46c1-9668-a145cbe1a6c9.jpeg",
         otherpic: [
             "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/7a2c948c-9081-46d9-b7d7-0524bce1f143.jpeg",
@@ -13,48 +12,25 @@ export const list = [
     },
     {
         title: "Nap A Lean",
-        link: "Nap_A_Lean",
         pic: "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/9e5ca743-3799-493a-9f56-794af2e7d980.jpeg",
         description: "ศิลปินดูโอ้จากเชียงใหม่ เจ้าของรางวัล พลัดถิ่นยอดเยี่ยม minimal awards #1 2013 โดยมีโต้มือกีต้าร์ Acoustic ผู้เป็นนักร้องนำ แล้วฮั้วมือกีต้าร์ไฟฟ้า ที่มาเจอกันในร้านอาหารที่ฮั้วทำงานอยู่ โดย ...",
         genres: ['POP']
     },
     {
-        title: "RIVER RHYME",
-        link: "RIVER_RHYME",
-        pic: "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/f1fc7ee5-48b6-46c1-9668-a145cbe1a6c9.jpeg",
+        title: "ZWEEDZ N' ROLL",
+        pic: "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/1d784562-ce8d-41b2-93b4-d272f378d0cf.jpeg",
+        description: "Zweedz n' roll (Alternative/Rock) Zweedz n' roll, an alternative band from the Faculty of Music, Silpakorn University, was founded in 2012 by a group of friends who have passion in Brit music. At first, we wanted to play in a pub at night just for fun, but after playing for several gigs, one of the pub owners inspired us to become much more. Since then we started to cover songs and post our clips on Youtube, which became a good channel for fans to follow us. Inspired by Brit Pop/Rock and alternative bands like The Beatles, The Cranberries, Radiohead and Nirvana, ZWEEDZ n' roll was named by New (the bassist). The name came from \"rock n' roll\" and \"sweet n' roll\" and finally became \"ZWEEDZ n' roll\" The debut of an MV \"December\" ZWEEDZ n' Roll วงอัลเตอร์เนทีฟ จากรั้วมหาลัยศิลปากร คณะดุริยางคศาสตร์ เริ่มก่อตั้งเมื่อปี 2012เป็นการรวมตัวของกลุ่มเพื่อนที่มีความหลงใหลดนตรีจากฝั่งอังกฤษ เพื่อเล่นดนตรีกลางคืน หลังจากนั้นก็ได้แกะเพลงตระเวณตามร้านต่างๆ ที่เปิดรับแนวดนตรีที่เราอยากจะเล่น จนได้Auditionที่ร้านๆนึง ความคิดของทางวงก็เริ่มเปลี่ยนไป จากการแนะนำของพี่เจ้าของร้าน หลังจากนั้นจึงเริ่มอัดคลิปCoverเพลงต่างๆ ลงเว็บไซต์ YOUTUBE และก็ได้มีผลงานของตัวเองในที่สุดคือเพลง \"ธันวาคม - zweedz n' roll\" Zweedz n' roll เป็นชื่อที่นิว(เบส) เป็นคนตั้งขึ้น เกิดจากการเล่นคำจาก rock n' roll >> sweet n' roll และเป็น zweedz n' roll ในที่สุด ได้รับอิทธิพลทางดนตรีหลักๆจากดนตรีแนว Brit Pop/Rock และAlternativeต่างๆ เช่น The Beatles ,The Cranberries ,Radiohead และ Nirvana เป็นต้น ประจำที่ PLAY YARD by Studio Bar ลาดพร้าวซอย8 วันพฤหัสเว้นพฤหัสนะ :-)",
+        genres: ['Alternative'],
         otherpic: [
-            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/7a2c948c-9081-46d9-b7d7-0524bce1f143.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/3efe484d-6117-48b9-89a9-68a69637c51f.jpeg",
             "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/5d8511aa-522d-4c3c-af17-32af8e4bbdb1.jpeg",
-            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/51cb1b16-7aea-40e6-b17c-1a9feabae613.jpeg"
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/51cb1b16-7aea-40e6-b17c-1a9feabae613.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/2c63410b-aeb3-4177-96d9-4ad4d2668806.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/8d9d315c-adbc-4346-9587-e0aa8576dd5c.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/be2585b9-484b-40a4-b74a-e51fec7440a6.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/8ed20559-4766-473a-924e-57f9b73bb914.jpeg",
+            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/7e9ccff2-5c3d-4668-b090-6d5529770087.jpeg"
         ],
-        description: "วุฒิชัย เอมแสง / Vutthichai Aemsang (จอห์น / John) aka. JOHNN: Rapper ธนภัทร พุทธกะ / Thanapat Putthaka (ต็อป / Stop) aka. BLACKSHEEPRR: Rapper นครินทร์ จรูญวิทยา /...",
-        genres: ['HipHop/Rap', 'R&B']
-    },
-    {
-        title: "Nap A Lean",
-        link: "Nap_A_Lean",
-        pic: "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/9e5ca743-3799-493a-9f56-794af2e7d980.jpeg",
-        description: "ศิลปินดูโอ้จากเชียงใหม่ เจ้าของรางวัล พลัดถิ่นยอดเยี่ยม minimal awards #1 2013 โดยมีโต้มือกีต้าร์ Acoustic ผู้เป็นนักร้องนำ แล้วฮั้วมือกีต้าร์ไฟฟ้า ที่มาเจอกันในร้านอาหารที่ฮั้วทำงานอยู่ โดย ...",
-        genres: ['POP']
-    },
-    {
-        title: "RIVER RHYME",
-        link: "RIVER_RHYME",
-        pic: "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/f1fc7ee5-48b6-46c1-9668-a145cbe1a6c9.jpeg",
-        otherpic: [
-            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/7a2c948c-9081-46d9-b7d7-0524bce1f143.jpeg",
-            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/5d8511aa-522d-4c3c-af17-32af8e4bbdb1.jpeg",
-            "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/51cb1b16-7aea-40e6-b17c-1a9feabae613.jpeg"
-        ],
-        description: "วุฒิชัย เอมแสง / Vutthichai Aemsang (จอห์น / John) aka. JOHNN: Rapper ธนภัทร พุทธกะ / Thanapat Putthaka (ต็อป / Stop) aka. BLACKSHEEPRR: Rapper นครินทร์ จรูญวิทยา /...",
-        genres: ['HipHop/Rap', 'R&B']
-    },
-    {
-        title: "Nap A Lean",
-        link: "Nap_A_Lean",
-        pic: "https://lensod-user-statics.s3-ap-southeast-1.amazonaws.com/9e5ca743-3799-493a-9f56-794af2e7d980.jpeg",
-        description: "ศิลปินดูโอ้จากเชียงใหม่ เจ้าของรางวัล พลัดถิ่นยอดเยี่ยม minimal awards #1 2013 โดยมีโต้มือกีต้าร์ Acoustic ผู้เป็นนักร้องนำ แล้วฮั้วมือกีต้าร์ไฟฟ้า ที่มาเจอกันในร้านอาหารที่ฮั้วทำงานอยู่ โดย ...",
-        genres: ['POP']
     },
 
 ]
